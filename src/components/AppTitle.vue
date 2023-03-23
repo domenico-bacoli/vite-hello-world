f1ffacdd7346d56fcae86848ad00f598f07eba35
domenicobacoli <domenico.bacoli89@gmail.com>
2023-03-23 15:50:09 +0100

    feat: add AppTitle components

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>La mia prima app con vite!</h1>
+</template>
+
+
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+</style>
